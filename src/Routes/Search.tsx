0ed6b707bfc32a871867e8c7217b54737f8f5102
hhshhhskk <hhshhhskk@naver.com>
2022-07-28 23:05:44 +0900

    Search Redirect

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router";
+
 function Search() {
-    return null;
-  }
-  export default Search;
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  searchParams.get("keyword");
+  return null;
+}
+export default Search;
